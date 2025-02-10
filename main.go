d587bd6c215321cacfa030fc3ce1194047461213
englandrecoil <nik.tereshchenko.982@gmail.com>
2025-02-11 01:40:03 +0300

    feat: add metrics tracking and initial endpoints - /metrics endpoint for tracking request counts - /reset endpoint to reset request count - /healthz endpoint for health checks - /app endpoint for main application

--- main.go
+++ main.go
@@ -1,1 +1,49 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func main() {
+	apiCfg := apiConfig{
+		fileServerHits: atomic.Int32{},
+	}
+
+	fileServer := http.FileServer(http.Dir("."))
+	strippedFileServer := http.StripPrefix("/app", fileServer)
+
+	mux := http.NewServeMux()
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(strippedFileServer))
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	log.Printf("Serving on port %s\n", server.Addr)
+	log.Fatal(server.ListenAndServe())
+
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileServerHits.Load())))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
